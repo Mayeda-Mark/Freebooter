5b329cbb8bb67f5f5fcbd6af84aad273cf6a3462
JapaneseFireBmb <markrmayeda@gmail.com>
2020-07-05 10:46:08 -0700

    got right cannon firing

---
+++ Free Booter/Assets/Scripts/Cannons.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cannons : MonoBehaviour
+{
+    [SerializeField] GameObject projectile, lCannon, rCannon;
+    GameObject projectileParent;
+    const string PROJECTILE_PARENT_NAME = "Projectiles";
+    // Start is called before the first frame update
+    float rCannonRotation;
+    void Start()
+    {
+        CreateProjectileParent();
+    }
+    private void CreateProjectileParent()
+    {
+        projectileParent = GameObject.Find(PROJECTILE_PARENT_NAME);
+        if (!projectileParent)
+        {
+            projectileParent = new GameObject(PROJECTILE_PARENT_NAME);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        FireCannon();
+    }
+
+    private void FireCannon()
+    {
+        if(Input.GetKeyUp(KeyCode.RightArrow)) {
+            print("Fire right cannon!");
+            GameObject newProjectile = Instantiate(projectile, rCannon.transform.position, rCannon.transform.rotation) as GameObject;
+            newProjectile.transform.parent = projectileParent.transform;
+        }
+    }
+}
