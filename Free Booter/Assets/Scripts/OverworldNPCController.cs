5590ce8c29855685a8e4de6836cbb39619960753
Mark Mayeda <markrmayeda@gmail.com>
2020-07-10 05:43:53 -0700

    got overworldNPCs moving

--- Free Booter/Assets/Scripts/OverworldNPCController.cs
+++ Free Booter/Assets/Scripts/OverworldNPCController.cs
@@ -5,16 +5,40 @@ using UnityEngine;
 public class OverworldNPCController : MonoBehaviour
 {
     [SerializeField] List<TownPortal> townPortals;
-    [SerializeField] List<OverWorldNPC> NPCs;
+    [SerializeField] float spawnDistance = 20f;
+    [SerializeField] float shipSpeed = 2f;
+    [SerializeField] GameObject NPC;
+    TownPortal target;
     // Start is called before the first frame update
     void Start()
     {
-        
+        SetTarget();
+    }
+    private void SetTarget() {
+        target = RollTarget();
+        if(Vector2.Distance(transform.position, target.transform.position) < spawnDistance) {
+            target = RollTarget();
+        }
+    }
+    private TownPortal RollTarget() {
+        int index = Random.Range(0, townPortals.Count);
+        return townPortals[index];
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        TurnTowardsTarget();
+        Move();   
+    }
+    private void TurnTowardsTarget() {
+        print("Target: " + target.transform.position);
+        print("NPC: " + NPC.transform.position);
+        Quaternion rotation = Quaternion.LookRotation(target.transform.position - NPC.transform.position, NPC.transform.TransformDirection(Vector3.up));
+        NPC.transform.rotation = new Quaternion(0, 0, rotation.z, rotation.w);
+    }
+    private void Move() {
+        float moveSpeed = shipSpeed * Time.deltaTime;
+        NPC.transform.position += transform.up * moveSpeed;
     }
 }
