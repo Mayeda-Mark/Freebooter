4d810fba005aef355b8797c7db2eff6a856b4fc3
Mark Mayeda <markrmayeda@gmail.com>
2020-07-16 09:16:03 -0700

    got NPCs traveling in desired direction

--- Free Booter/Assets/Scripts/OverworldNPCController.cs
+++ Free Booter/Assets/Scripts/OverworldNPCController.cs
@@ -7,16 +7,19 @@ public class OverworldNPCController : MonoBehaviour
     [SerializeField] List<TownPortal> townPortals;
     [SerializeField] float spawnDistance = 20f;
     [SerializeField] float shipSpeed = 2f;
+    [SerializeField] float turnSpeed = 50f;
     [SerializeField] GameObject NPC;
     TownPortal target;
-    // Start is called before the first frame update
+    Rigidbody2D myRigidBody;
+    
     void Start()
     {
+        myRigidBody = GetComponent<Rigidbody2D>();
         SetTarget();
     }
     private void SetTarget() {
         target = RollTarget();
-        if(Vector2.Distance(transform.position, target.transform.position) < spawnDistance) {
+        if(Vector2.Distance(transform.position, target.transform.position) <= spawnDistance) {
             target = RollTarget();
         }
     }
@@ -25,20 +28,19 @@ public class OverworldNPCController : MonoBehaviour
         return townPortals[index];
     }
 
-    // Update is called once per frame
     void Update()
     {
         TurnTowardsTarget();
         Move();   
     }
     private void TurnTowardsTarget() {
-        print("Target: " + target.transform.position);
-        print("NPC: " + NPC.transform.position);
-        Quaternion rotation = Quaternion.LookRotation(target.transform.position - NPC.transform.position, NPC.transform.TransformDirection(Vector3.up));
-        NPC.transform.rotation = new Quaternion(0, 0, rotation.z, rotation.w);
+        Vector2 dir = target.transform.position - transform.position;
+        float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        Quaternion rotation = Quaternion.Euler( 0, 0, angle - 90f);
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, turnSpeed * Time.deltaTime);
     }
     private void Move() {
         float moveSpeed = shipSpeed * Time.deltaTime;
-        NPC.transform.position += transform.up * moveSpeed;
+        transform.position += transform.up * moveSpeed;
     }
 }
