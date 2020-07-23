7941fce0305cbe2e56509b840e3a044ec24eb0f0
Mark Mayeda <markrmayeda@gmail.com>
2020-07-23 13:31:21 -0700

    Added instructions canvas

---
+++ Free Booter/Assets/Scripts/GameController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    [SerializeField] Canvas instructionCanvas;
+    // Start is called before the first frame update
+    void Start()
+    {
+        instructionCanvas.enabled = true;
+        Time.timeScale = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void StartButton() {
+        Time.timeScale = 1.0f;
+        instructionCanvas.enabled = false;
+    }
+}
