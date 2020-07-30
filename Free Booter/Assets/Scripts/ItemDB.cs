b637a59a342a309eb5b658818d7d65ccfabc840b
Mark Mayeda <markrmayeda@gmail.com>
2020-07-30 11:01:59 -0700

    Started working on inventory back end

---
+++ Free Booter/Assets/Scripts/ItemDB.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemDB
+{
+    public List<Item> items = new List<Item>();
+    void BuildDb() {
+        items = new List<Item>{
+            new Item(0, "Cannon Balls", "Big balls of metal that you can hurl at other boats", 
+            new Dictionary<string, int> {
+                {"Cost", 20}
+            })
+        };
+    }
+}
