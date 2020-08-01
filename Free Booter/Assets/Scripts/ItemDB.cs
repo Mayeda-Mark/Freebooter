1e47be89cd5d02d9bf6e57e9cc333f7af693c762
Mark Mayeda <markrmayeda@gmail.com>
2020-08-01 16:54:47 -0700

    Built ship inventory db and ui

--- Free Booter/Assets/Scripts/ItemDB.cs
+++ Free Booter/Assets/Scripts/ItemDB.cs
@@ -2,14 +2,31 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ItemDB
+public class ItemDB : MonoBehaviour
 {
-    public List<Item> items = new List<Item>();
+    [SerializeField] public List<Item> items = new List<Item>();
+    private void Awake() {
+        BuildDb();
+    }
+    public Item GetItem(int id) {
+        return items.Find(item => item.id == id);
+    }
+    public Item GetItem(string itemName) {
+        return items.Find(item => item.itemName == itemName);
+    }
     void BuildDb() {
         items = new List<Item>{
             new Item(0, "Cannon Balls", "Big balls of metal that you can hurl at other boats", 
             new Dictionary<string, int> {
-                {"Cost", 20}
+                {"Cost", 20},
+                {"HullDamage", 20},
+                {"SailDamage", 5}
+            }), 
+            new Item(1, "Chain Shot", "It's like a bolo, but for a boat", 
+            new Dictionary<string, int> {
+                {"Cost", 20},
+                {"HullDamage", 5},
+                {"SailDamage", 20}
             })
         };
     }
---
+++ Free Booter/Assets/Scripts/ShipInventory.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipInventory : MonoBehaviour
+{
+    private void Start() {
+        GiveItem("Cannon Balls");
+        RemoveItem(1);
+    }
+    public List<Item> shipItems = new List<Item>();
+    public ItemDB itemDB;
+    public void GiveItem(int id) {
+        Item itemToAdd = itemDB.GetItem(id);
+        shipItems.Add(itemToAdd);
+    }
+    public void GiveItem(string itemName) {
+        Item itemToAdd = itemDB.GetItem(itemName);
+        shipItems.Add(itemToAdd);
+    }
+    public Item CheckForItem(int id) {
+        return shipItems.Find(item => item.id == id);
+    }
+    public void RemoveItem(int id) {
+        Item item = CheckForItem(id);
+        if(item != null) {
+            shipItems.Remove(item);
+            Debug.Log("Removed " + item.itemName);
+        } else {
+            Debug.Log("Item not found");
+        }
+    }
+}
