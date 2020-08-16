48a00aaf082413f37b93e12a19d9aeb7359ad102
Mark Mayeda <markrmayeda@gmail.com>
2020-08-16 00:51:24 -0700

    Worked on towns some more

--- Free Booter/Assets/Scripts/TownController.cs
+++ Free Booter/Assets/Scripts/TownController.cs
@@ -6,16 +6,27 @@ using UnityEngine.UI;
 public class TownController : MonoBehaviour
 {
     public Canvas townCanvas;
+    public GameObject townMenu;
+    public GameObject merchantMenu;
     void Start() {
         townCanvas.enabled = false;
     }
     public void OpenMenu() {
         townCanvas.enabled = true;
+        merchantMenu.gameObject.SetActive(false);
         Time.timeScale = 0f;
     }
     public void CloseMenu() {
         townCanvas.enabled = false;
         Time.timeScale = 1.0f;
+    }
+    public void OpenMerchant() {
+        townMenu.gameObject.SetActive(false);
+        merchantMenu.gameObject.SetActive(true);
+    }
+    public void CloseMerchant() {
+        townMenu.gameObject.SetActive(true);
+        merchantMenu.gameObject.SetActive(false);
     }
     public void RepairShip() {
         FindObjectOfType<PlayerShipController>().GetComponent<Health>().ResetHealth();
