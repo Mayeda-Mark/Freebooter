6833923a08d9d389367ab94f884287d64e0b2a1b
Mark Mayeda <markrmayeda@gmail.com>
2020-09-19 10:44:13 -0700

    Added weather area

---
+++ Free Booter/Assets/Scripts/WeatherArea.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeatherArea : MonoBehaviour
+{
+    [SerializeField] Collider2D myCollider;
+    [SerializeField] List<int> weatherIds;
+    [SerializeField] WeatherDB weatherDb;
+    float weatherTimer;
+    int weatherIndex;
+    bool playing = true;
+    Weather currentWeather;
+    IEnumerator Start() {
+        do
+        {
+            yield return StartCoroutine("SetWeather");
+        }
+        while (playing);
+    }
+    private IEnumerator SetWeather()
+    {
+        yield return new WaitForSeconds(weatherTimer);
+        RollWeather();
+        //currentWeather = weatherDb.GetWeather(weatherIndex);
+    }
+    private void RollWeather() {
+        currentWeather = weatherDb.GetWeather(UnityEngine.Random.Range(0, weatherIds.Count));
+        weatherTimer = UnityEngine.Random.Range(60f, 180f);
+    }
+    private void OnTriggerStay2D(Collider2D collision) {
+        if(collision.GetComponent<PlayerShipController>()) {
+           Debug.Log("Weather: " + currentWeather.name);
+        }
+    }
+}
