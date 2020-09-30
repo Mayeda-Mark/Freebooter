4e8a69c899ee16404824b11df0538a3b185416b2
mmayeda <markrmayeda@gmail.com>
2020-09-30 12:16:50 -0700

    Continued working on storm systems

--- Free Booter/Assets/Scripts/StormArea.cs
+++ Free Booter/Assets/Scripts/StormArea.cs
@@ -5,36 +5,22 @@ using UnityEngine;
 
 public class StormArea : MonoBehaviour
 {
-    public float areaWidthHeight = 200;
-    public int intensity = 40;
-    public GameObject rainDropPrefab;
-    public GameObject parent;
-    float width, height, xPos, yPos;
-    //Mesh mesh;
-    //public RectTransform rt;
-    // Start is called before the first frame update
+    public int maxIntensity = 300;
+    public int minIntensity = 200;
+    public float windDir;
+    public float windIntensity;
+    ParticleSystem rainMaker;
     void Start() {
-        width = parent.GetComponent<Collider2D>().bounds.size.x;
-        height = parent.GetComponent<Collider2D>().bounds.size.y;
-        xPos = transform.position.x;
-        yPos = transform.position.y;
-        //Transform parentTransform = parent.transform;
-        //rt = (RectTransform)this.transform;
+        rainMaker = GetComponent<ParticleSystem>();
+        var em = rainMaker.emission.rate;
+        em.mode = ParticleSystemCurveMode.Constant;
+        em.constantMax = maxIntensity;
+        em.constantMin = minIntensity;
+        windDir = UnityEngine.Random.Range(0f, 306f);
+        //this.transform.rotation.eulerAngles z = windDir;
     }
 
     // Update is called once per frame
     void Update() {
-        CreateRainDrop();
     }
-    private void CreateRainDrop() {
-        for(int i = 0; i < intensity; i++){
-            Transform raindropTransform = this.transform;
-            /*Vector3 rainDropPos*/raindropTransform.position = new Vector3(/*0, 0*/xPos - areaWidthHeight + UnityEngine.Random.Range(0f, width + areaWidthHeight * 2), (yPos - areaWidthHeight + UnityEngine.Random.Range(0f, height + areaWidthHeight * 2)), 0);
-            print(raindropTransform.position);
-            GameObject rainDrop = Instantiate(rainDropPrefab, raindropTransform) as GameObject;
-        }
-    }
-
-    public float GetWidth() { return width;  }
-    public float GetHeight() { return height; }
 }
