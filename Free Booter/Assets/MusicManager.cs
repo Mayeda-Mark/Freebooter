682d2ee2f6dfa22de6ae8658bedd3bc5f2832757
mmayeda <markrmayeda@gmail.com>
2020-11-21 10:15:13 -0800

    Started on sounds/music

---
+++ Free Booter/Assets/MusicManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour
+{
+    [SerializeField] Dictionary<string, AudioSource> tracks = new Dictionary<string, AudioSource>();
+
+    bool isPlaying = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void ChangeTrack()
+    {
+
+    }
+}
