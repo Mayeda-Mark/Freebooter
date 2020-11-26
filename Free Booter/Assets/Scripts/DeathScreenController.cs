11430534201dcb14be0e5ec98d8fdec3d24d3ba8
mmayeda <markrmayeda@gmail.com>
2020-11-26 10:37:48 -0800

    Made death screen

---
+++ Free Booter/Assets/Scripts/DeathScreenController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathScreenController : MonoBehaviour
+{
+    LevelLoader levelLoader;
+    void Start()
+    {
+        levelLoader = FindObjectOfType<LevelLoader>();
+    }
+    public void Main()
+    {
+        levelLoader.LoadMainMenu();
+    }
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
