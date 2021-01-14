574b64848d52ca1f73881ac933335e8dd11f96e2
mmayeda <markrmayeda@gmail.com>
2021-01-14 10:54:58 -0800

    COntinued working on sidescroll setup

---
+++ Free Booter/Assets/SidescrollController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SidescrollController : MonoBehaviour
+{
+    private void Awake()
+    {
+        Physics2D.gravity = new Vector2(0, -9);
+        
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
