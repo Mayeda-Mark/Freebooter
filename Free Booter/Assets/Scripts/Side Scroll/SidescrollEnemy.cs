a2173e26a41e30a7600594e8a597fc2d665d90fa
mmayeda <markrmayeda@gmail.com>
2021-01-23 10:16:30 -0800

    Worked on enemy patrol

--- Free Booter/Assets/Scripts/Side Scroll/SidescrollEnemy.cs
+++ Free Booter/Assets/Scripts/Side Scroll/SidescrollEnemy.cs
@@ -3,10 +3,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SidescrollNPC : MonoBehaviour
+public class SidescrollEnemy : MonoBehaviour
 {
     #region Public Variables
-    public Transform rayCast;
+    public Transform rayCast, leftLimit, rightLimit;
     public LayerMask rayCastMask;
     public float rayCastLength;
     public float attackDistance;
@@ -15,44 +15,57 @@ public class SidescrollNPC : MonoBehaviour
     #endregion
     #region Private Variables
     private RaycastHit2D hit;
-    private GameObject target;
+    private Transform target;
     private Animator anim;
     private float distance;
     private bool attackMode;
     private bool inRange;
     private bool inCooldown;
     private float intTimer;
+    private Rigidbody2D myRigidBody;
     #endregion
     private void Awake()
     {
+        SelectTarget();
+        myRigidBody = GetComponent<Rigidbody2D>();
         intTimer = attackTimer;
         anim = GetComponent<Animator>();
     }
     void Update()
     {
+        if(!attackMode)
+        {
+            Move();
+        }
+        if(!InsideLimits() && !inRange && !anim.GetCurrentAnimatorStateInfo(0).IsName("Minotaur_Attack"))
+        {
+            SelectTarget();
+        }
         if(inRange)
         {
-            hit = Physics2D.Raycast(rayCast.position, Vector2.left, rayCastLength, rayCastMask);
+            hit = Physics2D.Raycast(rayCast.position, transform.right, rayCastLength, rayCastMask);
             RayCastDebugger();
-        }else if(!inRange)        {
-            anim.SetBool("isWalking", false);
-            StopAttack();
         }
         if(hit.collider != null)
         {
             EnemyLogic();
-        } else if (hit.collider == null)
+        }
+        else /*if (hit.collider == null)*/
         {
+            print("Called!");
             inRange = false;
+        }
+        if(inRange == false)
+        {
+            StopAttack();
         }
     }
 
     private void EnemyLogic()
     {
-        distance = Vector2.Distance(transform.position, target.transform.position);
+        distance = Vector2.Distance(transform.position, target.position);
         if(distance > attackDistance)
         {
-            Move();
             StopAttack();
         } else if(attackDistance >= distance && !inCooldown)
         {
@@ -69,7 +82,7 @@ public class SidescrollNPC : MonoBehaviour
         anim.SetBool("isWalking", true);
         if(!anim.GetCurrentAnimatorStateInfo(0).IsName("Minotaur_Attack"))
         {
-            Vector2 targetPosition = new Vector2(target.transform.position.x, transform.position.y);
+            Vector2 targetPosition = new Vector2(target.position.x, transform.position.y);
             transform.position = Vector2.MoveTowards(transform.position, targetPosition, moveSpeed * Time.deltaTime);
         }
     }
@@ -99,11 +112,11 @@ public class SidescrollNPC : MonoBehaviour
     {
         if(distance > attackDistance)
         {
-            Debug.DrawRay(rayCast.position, Vector2.left * rayCastLength, Color.red);
+            Debug.DrawRay(rayCast.position, transform.right * rayCastLength, Color.red);
         }
         else if(attackDistance > distance)
         {
-            Debug.DrawRay(rayCast.position, Vector2.left * rayCastLength, Color.green);
+            Debug.DrawRay(rayCast.position, transform.right * rayCastLength, Color.green);
         }
     }
 
@@ -111,12 +124,51 @@ public class SidescrollNPC : MonoBehaviour
     {
         if(trig.gameObject.tag == "Player")
         {
-            target = trig.gameObject;
+            target = trig.transform;
             inRange = true;
+            FLip();
         }
     }
     public void TriggerCooling()
     {
         inCooldown = true;
+    }
+    private bool InsideLimits()
+    {
+        return transform.position.x > leftLimit.position.x && transform.position.x < rightLimit.position.x;
+    }
+    private void SelectTarget()
+    {
+        float distanceToLeft = Vector2.Distance(transform.position, leftLimit.position);
+        float distanceToRight = Vector2.Distance(transform.position, rightLimit.position);
+
+        if(distanceToLeft > distanceToRight)
+        {
+            target = leftLimit;
+        } else
+        {
+            target = rightLimit;
+        }
+        FLip();
+    }
+
+    private void FLip()
+    {
+        /*bool enemyHasHorizontalSpeed = Mathf.Abs(myRigidBody.velocity.x) > Mathf.Epsilon;
+        if (enemyHasHorizontalSpeed)
+        {
+            transform.localScale = new Vector2(Mathf.Sign(myRigidBody.velocity.x), 1f);
+        }*/
+
+        Vector3 rotation = transform.eulerAngles;
+        if (transform.position.x > target.position.x)
+        {
+            rotation.y = 180f;
+        }
+        else
+        {
+            rotation.y = 0f;
+        }
+        transform.eulerAngles = rotation;
     }
 }
