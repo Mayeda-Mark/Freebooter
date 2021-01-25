60a778158f977c4525233affb7fa6e4c9f1de14c
mmayeda <markrmayeda@gmail.com>
2021-01-25 12:24:47 -0800

    Added knockback mechanic

---
+++ Free Booter/Assets/Scripts/Side Scroll/DamageDetector.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageDetector : MonoBehaviour
+{
+    public Rigidbody2D myRigidBody;
+    public float knockBackH, knockBackV;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        DamageDealer dealer = collision.GetComponent<DamageDealer>();
+        if(dealer != null)
+        {
+            /*Vector2 knockBackVelocity = new Vector2(knockBackH, knockBackV);
+            myRigidBody.velocity = knockBackVelocity;*/
+            myRigidBody.AddForce(new Vector2(knockBackH, knockBackV));
+        }
+    }
+}
