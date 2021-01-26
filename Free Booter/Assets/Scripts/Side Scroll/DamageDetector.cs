04457f1e24906ee9e1048588f24cb4f1f4c85558
mmayeda <markrmayeda@gmail.com>
2021-01-26 09:26:25 -0800

    Got knockback working

--- Free Booter/Assets/Scripts/Side Scroll/DamageDetector.cs
+++ Free Booter/Assets/Scripts/Side Scroll/DamageDetector.cs
@@ -6,6 +6,7 @@ public class DamageDetector : MonoBehaviour
 {
     public Rigidbody2D myRigidBody;
     public float knockBackH, knockBackV;
+    public PlayerSidescrollController player;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         DamageDealer dealer = collision.GetComponent<DamageDealer>();
@@ -13,6 +14,7 @@ public class DamageDetector : MonoBehaviour
         {
             /*Vector2 knockBackVelocity = new Vector2(knockBackH, knockBackV);
             myRigidBody.velocity = knockBackVelocity;*/
+            player.SetKnockBack();
             myRigidBody.AddForce(new Vector2(knockBackH, knockBackV));
         }
     }
--- Free Booter/Assets/Scripts/Side Scroll/PlayerSidescrollController.cs
+++ Free Booter/Assets/Scripts/Side Scroll/PlayerSidescrollController.cs
@@ -8,11 +8,13 @@ public class PlayerSidescrollController : MonoBehaviour
     [SerializeField] float runSpeed = 8f;
     [SerializeField] float jumpSpeed = 15f;
     [SerializeField] float climbSpeed = 8f;
+    [SerializeField] float knockBackTimer = 0.25f;
+    float startingKnockBackTimer;
     Rigidbody2D myRigidBody;
     Animator myAnimator;
     [SerializeField] BoxCollider2D myFeet, ledgeCatcher;
     //[SerializeField] BoxCollider2D frontOfBody;
-    [HideInInspector] public bool isRunning, isJumping, isFalling, isBlocking, isCrouching, isAttacking, isSliding, isHanging, isClimbingLedge;
+    [HideInInspector] public bool isRunning, isJumping, isFalling, isBlocking, isCrouching, isAttacking, isSliding, isHanging, isClimbingLedge, knockBack;
 
     internal void Death()
     {
@@ -22,6 +24,9 @@ public class PlayerSidescrollController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        startingKnockBackTimer = knockBackTimer;
+        knockBack = false;
+        ResetKnockBackTimer();
         myRigidBody = GetComponent<Rigidbody2D>();
         myAnimator = GetComponent<Animator>();
         myFeet = GetComponent<BoxCollider2D>();
@@ -38,6 +43,7 @@ public class PlayerSidescrollController : MonoBehaviour
         Jump();
         EndFall();
         Block();
+        KnockBackTimer();
         /*CatchLedge();
         ClimbLedge();*/
     }
@@ -65,7 +71,7 @@ public class PlayerSidescrollController : MonoBehaviour
 
     private void Run()
     {
-        if (!isAttacking && !isCrouching && !isHanging)
+        if (!isAttacking && !isCrouching && !isHanging && !knockBack)
         {
             float controlThrow = Input.GetAxis("Horizontal");
             Vector2 playerVelocity = new Vector2(controlThrow * runSpeed, myRigidBody.velocity.y);
@@ -165,86 +171,108 @@ public class PlayerSidescrollController : MonoBehaviour
         Vector2 playerVelocity = new Vector2(0, 0);
         myRigidBody.velocity = playerVelocity;
     }
-    /*
-     * public class Player : MonoBehaviour {
-        //Config
-        [SerializeField] float runSpeed = 8f;
-        [SerializeField] float jumpSpeed = 15f;
-        [SerializeField] float climbSpeed = 8f;
-        float deathVertical = 25f;
-        float deathHorizontal = -100f;
-        //State
-        bool isAlive = true;
-        //Cached vars
-        Rigidbody2D myRigidBody;
-        Animator myAnimator;
-        CapsuleCollider2D myBodyCollider;
-        BoxCollider2D myFeet;
-        GameSession gameSession;
-        float playerGravity;
-
-        void Start () {
-            myFeet = GetComponent<BoxCollider2D>();
-            myRigidBody = GetComponent<Rigidbody2D>();
-            myAnimator = GetComponent<Animator>();
-            myBodyCollider = GetComponent<CapsuleCollider2D>();
-            playerGravity = myRigidBody.gravityScale;
-            gameSession = FindObjectOfType<GameSession>();
-        }
-
-        void Update () {
-            if(!isAlive) { return; }
-            Run();
-            Jump();
-            Climb();
-            FlipSprite();
-            Die();
-        }
-        private void Run() {
-            float controlThrow = Input.GetAxis("Horizontal");
-            Vector2 playerVelocity = new Vector2( controlThrow * runSpeed, myRigidBody.velocity.y);
-            myRigidBody.velocity = playerVelocity;
-        }
-
-        private void Jump() {
-            if(!myFeet.IsTouchingLayers(LayerMask.GetMask("Ground"))) { return; }
-            bool playerHasVerticalSpeed  = Mathf.Abs(myRigidBody.velocity.y) > Mathf.Epsilon;
-            if(Input.GetButtonDown("Jump")) {
-                Vector2 jumpVelocity = new Vector2(0f, jumpSpeed);
-                myRigidBody.velocity += jumpVelocity;
-            }
-            // myAnimator.SetBool("isJumping", playerHasVerticalSpeed);
-        }
-        private void Climb() {
-            if(!myFeet.IsTouchingLayers(LayerMask.GetMask("Climbable"))) { 
-                myAnimator.SetBool("isClimbing", false);
-                myRigidBody.gravityScale = playerGravity;
-                return; 
-            }
-            myRigidBody.gravityScale = 0;
-            float controlThrow = Input.GetAxis("Vertical");
-            Vector2 playerVelocity = new Vector2(myRigidBody.velocity.x, controlThrow * climbSpeed);
-            myRigidBody.velocity = playerVelocity;
-            bool playerHasVerticalSpeed  = Mathf.Abs(myRigidBody.velocity.y) > Mathf.Epsilon;
-            myAnimator.SetBool("isClimbing", playerHasVerticalSpeed);
-        }
-        private void FlipSprite() {
-            bool playerHasHorizontalSpeed  = Mathf.Abs(myRigidBody.velocity.x) > Mathf.Epsilon;
-            if (playerHasHorizontalSpeed) {
-                transform.localScale = new Vector2(Mathf.Sign(myRigidBody.velocity.x), 1f);
-            }
-            myAnimator.SetBool("isRunning", playerHasHorizontalSpeed);
-        }
-        private void Die() {
-            if(myBodyCollider.IsTouchingLayers(LayerMask.GetMask("Enemy", "Hazards"))){
-                Vector2 DeathVelocity = new Vector2(deathHorizontal, deathVertical);
-                myRigidBody.velocity = DeathVelocity;
-                myAnimator.SetBool("hasDied", true);
-                isAlive = false;
-                gameSession.ProcessPlayerDeath();
-            }
-        }
+    public void SetKnockBack()
+    {
+        knockBack = true;
+        ResetKnockBackTimer();
     }
+    private void ResetKnockBackTimer()
+    {
+        knockBackTimer = startingKnockBackTimer;
+    }
+    private void KnockBackTimer()
+    {
+        if(knockBack)
+        {
+            knockBackTimer -= Time.deltaTime;
+            if(knockBackTimer <= 0)
+            {
+                knockBack = false;
+                ResetKnockBackTimer();
+            }
+        }
+        
+    }
+    /*
+* public class Player : MonoBehaviour {
+   //Config
+   [SerializeField] float runSpeed = 8f;
+   [SerializeField] float jumpSpeed = 15f;
+   [SerializeField] float climbSpeed = 8f;
+   float deathVertical = 25f;
+   float deathHorizontal = -100f;
+   //State
+   bool isAlive = true;
+   //Cached vars
+   Rigidbody2D myRigidBody;
+   Animator myAnimator;
+   CapsuleCollider2D myBodyCollider;
+   BoxCollider2D myFeet;
+   GameSession gameSession;
+   float playerGravity;
 
-     */
+   void Start () {
+       myFeet = GetComponent<BoxCollider2D>();
+       myRigidBody = GetComponent<Rigidbody2D>();
+       myAnimator = GetComponent<Animator>();
+       myBodyCollider = GetComponent<CapsuleCollider2D>();
+       playerGravity = myRigidBody.gravityScale;
+       gameSession = FindObjectOfType<GameSession>();
+   }
+
+   void Update () {
+       if(!isAlive) { return; }
+       Run();
+       Jump();
+       Climb();
+       FlipSprite();
+       Die();
+   }
+   private void Run() {
+       float controlThrow = Input.GetAxis("Horizontal");
+       Vector2 playerVelocity = new Vector2( controlThrow * runSpeed, myRigidBody.velocity.y);
+       myRigidBody.velocity = playerVelocity;
+   }
+
+   private void Jump() {
+       if(!myFeet.IsTouchingLayers(LayerMask.GetMask("Ground"))) { return; }
+       bool playerHasVerticalSpeed  = Mathf.Abs(myRigidBody.velocity.y) > Mathf.Epsilon;
+       if(Input.GetButtonDown("Jump")) {
+           Vector2 jumpVelocity = new Vector2(0f, jumpSpeed);
+           myRigidBody.velocity += jumpVelocity;
+       }
+       // myAnimator.SetBool("isJumping", playerHasVerticalSpeed);
+   }
+   private void Climb() {
+       if(!myFeet.IsTouchingLayers(LayerMask.GetMask("Climbable"))) { 
+           myAnimator.SetBool("isClimbing", false);
+           myRigidBody.gravityScale = playerGravity;
+           return; 
+       }
+       myRigidBody.gravityScale = 0;
+       float controlThrow = Input.GetAxis("Vertical");
+       Vector2 playerVelocity = new Vector2(myRigidBody.velocity.x, controlThrow * climbSpeed);
+       myRigidBody.velocity = playerVelocity;
+       bool playerHasVerticalSpeed  = Mathf.Abs(myRigidBody.velocity.y) > Mathf.Epsilon;
+       myAnimator.SetBool("isClimbing", playerHasVerticalSpeed);
+   }
+   private void FlipSprite() {
+       bool playerHasHorizontalSpeed  = Mathf.Abs(myRigidBody.velocity.x) > Mathf.Epsilon;
+       if (playerHasHorizontalSpeed) {
+           transform.localScale = new Vector2(Mathf.Sign(myRigidBody.velocity.x), 1f);
+       }
+       myAnimator.SetBool("isRunning", playerHasHorizontalSpeed);
+   }
+   private void Die() {
+       if(myBodyCollider.IsTouchingLayers(LayerMask.GetMask("Enemy", "Hazards"))){
+           Vector2 DeathVelocity = new Vector2(deathHorizontal, deathVertical);
+           myRigidBody.velocity = DeathVelocity;
+           myAnimator.SetBool("hasDied", true);
+           isAlive = false;
+           gameSession.ProcessPlayerDeath();
+       }
+   }
+}
+
+*/
 }
